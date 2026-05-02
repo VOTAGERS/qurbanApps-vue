cb08918a48a75c1d83226c678c05a4d2382f1cbe
imurra <imamnc.developer@gmail.com>
2026-05-02 20:25:36 +0700

    adjust to Move to admin path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+// Layout
 import AdminLayout from '../layouts/AdminLayout.vue'
-import HomeView from '../views/HomeView.vue'
-// Import the new payment view
-import PaymentView from '../views/PaymentView.vue'
+
+// Pages
+import Landing from '../views/Landing.vue'
+import HomeView from '../views/admin/HomeView.vue'
+import PaymentView from '../views/admin/PaymentView.vue'
+
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // ✅ LANDING PAGE (tanpa admin layout)
     {
-      path: '/',
+      path: '',
+      name: 'landing',
+      component: Landing,
+    },
+
+    // ✅ ADMIN AREA
+    {
+      path: '/admin',
       component: AdminLayout,
       children: [
         {
-          path: '',
+          path: 'checkout',
           name: 'home',
           component: HomeView,
         },
         {
           path: 'products-detail',
           name: 'products-detail',
-          component: () => import('../views/ProductDetailSetupView.vue'),
+          component: () => import('../views/admin/ProductDetailSetupView.vue'),
         },
-
         {
           path: 'orders',
           name: 'orders',
-          component: () => import('../views/OrderListView.vue'),
+          component: () => import('../views/admin/OrderListView.vue'),
         },
         {
           path: 'my-orders',
           name: 'my-orders',
-          component: () => import('../views/MyOrderView.vue'),
+          component: () => import('../views/admin/MyOrderView.vue'),
         },
         // Add the new payment route
         {
@@ -45,6 +57,40 @@ const router = createRouter({
       ]
     }
   ],
+
+  // routes: [
+  //   {
+  //     path: '/',
+  //     component: AdminLayout,
+  //     children: [
+  //       {
+  //         path: '',
+  //         name: 'home',
+  //         component: HomeView,
+  //       },
+  //       {
+  //         path: 'products-detail',
+  //         name: 'products-detail',
+  //         component: () => import('../views/ProductDetailSetupView.vue'),
+  //       },
+
+  //       {
+  //         path: 'orders',
+  //         name: 'orders',
+  //         component: () => import('../views/OrderListView.vue'),
+  //       },
+  //       // Add the new payment route
+  //       {
+  //         path: 'payment',
+  //         name: 'payment',
+  //         component: PaymentView,
+  //         props: (route) => ({
+  //           order_code: route.query.order_code
+  //         })
+  //       }
+  //     ]
+  //   }
+  // ],
 })
 
 export default router
