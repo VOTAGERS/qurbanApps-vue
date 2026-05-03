28d4c05b13dd0a9da31ab98efb2e947f366834f2
Tony <46844297+Tonyprasteyo2@users.noreply.github.com>
2026-05-03 09:17:06 +0700

    landing baru

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AdminLayout from '../layouts/AdminLayout.vue'
 import { dashboardRoutes } from './routes/dashboard'
+import LandingPage from '@/views/LandingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+       path: '/',
+       component:LandingPage
+    },
+    {
+      path: '/admin',
       component: AdminLayout,
       children: [
         ...dashboardRoutes
